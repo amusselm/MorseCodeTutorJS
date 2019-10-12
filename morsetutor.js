b519ae8365b97d592b7632b273d097a31fd76f41
Andrew J. Musselman <ki6uoc@gmail.com>
2019-10-11 21:09:40 -0700

    Fixes issue with min/max volume numbers & more UI
    It turns out that if you set the min and max to the same number or conflicting
    numbers, it causes issues.

--- morsetutor.js
+++ morsetutor.js
@@ -6,6 +6,11 @@ var updateVolume = function(volumeInput) {
     tone.volume = volumeInput; 
 }
 
+
+var updateSendSpeed = function(sendSpeedInput) {
+    sendOptions.speed = sendSpeedInput;
+}
+
 var tone = {
     frequency : 440,
     volume : 15,
@@ -17,27 +22,47 @@ var tone = {
     },
 
     set volume(newVolume) {
-	alert("Hey!");
-	document.getElementById("volumeRange").value = newVolume;
-	document.getElementById("volumeNum").value = newVolume;
+        document.getElementById("volumeRange").value = newVolume;
+        document.getElementById("volumeNum").value = newVolume;
     }
 
 }
 
-var frequencyMin = 15;
-var frequencyMax = 2000;
-document.getElementById("frequencyRange").min = frequencyMin;
-document.getElementById("frequencyNum").min = frequencyMin;
-document.getElementById("frequencyRange").max = frequencyMax;
-document.getElementById("frequencyNum").max = frequencyMax;
+var sendOptions = {
+    speed : 15,
+
+    set speed(newSpeed) {
+        document.getElementById("sendSpeedRange").value = newSpeed;
+        document.getElementById("sendSpeedNum").value = newSpeed;
+    }
+}
+
+function init() {
+    var frequencyMin = 15;
+    var frequencyMax = 2000;
+    document.getElementById("frequencyRange").min = frequencyMin;
+    document.getElementById("frequencyNum").min = frequencyMin;
+    document.getElementById("frequencyRange").max = frequencyMax;
+    document.getElementById("frequencyNum").max = frequencyMax;
+
+
+    var maxVolume = 100;
+    var minVolume = 0;
+    document.getElementById("volumeRange").max = maxVolume;
+    document.getElementById("volumeNum").min = minVolume;
+    document.getElementById("volumeRange").max = maxVolume;
+    document.getElementById("volumeNum").min = minVolume;
+
+    var maxSendSpeed = 150;
+    var minSendSpeed = 5;
+    document.getElementById("sendSpeedRange").min = minSendSpeed;
+    document.getElementById("sendSpeedRange").max = maxSendSpeed;
+    document.getElementById("sendSpeedNum").min = minSendSpeed;
+    document.getElementById("sendSpeedNum").max = maxSendSpeed;
+}
+
+init();
 
 tone.frequency = 440;
-
-var maxVolume = 100;
-var minVolume = 0;
-document.getElementById("volumeRange").min = maxVolume;
-document.getElementById("volumeNum").min = minVolume;
-document.getElementById("volumeRange").max = maxVolume;
-document.getElementById("volumeNum").max = minVolume;
-
 tone.volume = 50;
+sendOptions.speed = 15;
