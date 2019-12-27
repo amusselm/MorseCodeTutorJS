c95f067057362067ee4ead0105a02f7fe9da3080
Andrew J. Musselman <ki6uoc@gmail.com>
2019-12-27 06:47:16 -0800

    Started Using Javascript Namespace pattern
    As recommended in the W3C's Javascript best practices
    (See https://www.w3.org/wiki/JavaScript_best_practices)
    I'm encapuslating all the code into its own namespace
    to avoid possible namespace collisions with other Javascript
    files that may be included in the future.

--- morsetutor.js
+++ morsetutor.js
@@ -1,152 +1,167 @@
-var updateFrequency = function(toneInput) {
-    tone.frequency = toneInput; 
-}
-
-var updateVolume = function(volumeInput) {
-    tone.volume = volumeInput; 
-}
-
-var updateSendSpeed = function(sendSpeedInput) {
-    sendOptions.speed = sendSpeedInput;
-}
-
-var updateGroupSize = function(sendGroupInput) {
-    sendOptions.groupSize = sendGroupInput;
-}
-
-var updateGroupCount = function(sendGroupCountInput) {
-    sendOptions.groupCount = sendGroupCountInput;
-}
-
-var tone = {
-    frequency : 440,
-    volume : 15,
-    frequencyMin : 15,
-    frequencyMax : 2000,
-    maxVolume : 100,
-    minVolume : 0,
-
-    set frequency(newFreq) {
-        document.getElementById("frequencyRange").value = newFreq;
-        document.getElementById("frequencyNum").value = newFreq;
-        
-    },
-
-    set volume(newVolume) {
-        document.getElementById("volumeRange").value = newVolume;
-        document.getElementById("volumeNum").value = newVolume;
+var morseTutorUi = function() {
+    var updateFrequency = function(toneInput) {
+        tone.frequency = toneInput; 
     }
 
-}
-
-var sendOptions = {
-    speed : 15,
-    maxSendSpeed : 150,
-    minSendSpeed : 5,
-    groupSize: 3,
-    minGroupSize: 1,
-    maxGroupSize: 20,
-    groupCount: 1,
-    minGroupCount: 1,
-    maxGroupCount: 20,
-
-    set speed(newSpeed) {
-        document.getElementById("sendSpeedRange").value = newSpeed;
-        document.getElementById("sendSpeedNum").value = newSpeed;
+    var updateVolume = function(volumeInput) {
+        tone.volume = volumeInput; 
     }
-}
 
-
-
-function Letter(display, morse) {
-    this.display = display;
-    this.morse = morse;
-    this.enabled = false; 
-}
-
-Letter.prototype.getInnerHtml = function() {
-    let listTag = document.createElement("li");
-    listTag.innerHTML = this.display;
-
-    let inputTag = document.createElement("input");
-    inputTag.setAttribute('type','checkbox');
-    inputTag.addEventListener('click',this.toggle.bind(this));
-    listTag.appendChild(inputTag);
-    return listTag;
-};
-
-Letter.prototype.toggle = function() {
-    this.enabled = !this.enabled;
-    if (this.enabled) {
-        enabledAlphabet.push(this); 
-        console.log(this);
-    }  else {
-        let idx = enabledAlphabet.indexOf(this);
-        enabledAlphabet.splice(idx, 1);
+    var updateSendSpeed = function(sendSpeedInput) {
+        sendOptions.speed = sendSpeedInput;
     }
-    console.log(enabledAlphabet);
 
-};
-
-var alphabet = [
-    new Letter("a",".-"),
-    new Letter("b","-..."),
-    new Letter("c","-.-."),
-    new Letter("d","-.."),
-    new Letter("e","."),
-    new Letter("f","..-.")
-];
-
-var enabledAlphabet = [];
-
-function sendRandomLetter() {
-    if (enabledAlphabet.length > 0) {
-        let rand = Math.random();
-        let scaledRand = rand * enabledAlphabet.length;
-        let idx = Math.floor(scaledRand);
-        console.log(enabledAlphabet[idx]);
-        alert(enabledAlphabet[idx].display);
-    } else {
-        alert("No letters selected");
+    var updateGroupSize = function(sendGroupInput) {
+        sendOptions.groupSize = sendGroupInput;
     }
-}
 
-function init() {
-    document.getElementById("frequencyRange").min = tone.frequencyMin;
-    document.getElementById("frequencyNum").min = tone.frequencyMin;
-    document.getElementById("frequencyRange").max = tone.frequencyMax;
-    document.getElementById("frequencyNum").max = tone.frequencyMax;
+    var updateGroupCount = function(sendGroupCountInput) {
+        sendOptions.groupCount = sendGroupCountInput;
+    }
+
+    var tone = {
+        frequency : 440,
+        volume : 15,
+        frequencyMin : 15,
+        frequencyMax : 2000,
+        maxVolume : 100,
+        minVolume : 0,
+
+        set frequency(newFreq) {
+            document.getElementById("frequencyRange").value = newFreq;
+            document.getElementById("frequencyNum").value = newFreq;
+            
+        },
+
+        set volume(newVolume) {
+            document.getElementById("volumeRange").value = newVolume;
+            document.getElementById("volumeNum").value = newVolume;
+        }
+
+    }
+
+    var sendOptions = {
+        speed : 15,
+        maxSendSpeed : 150,
+        minSendSpeed : 5,
+        groupSize: 3,
+        minGroupSize: 1,
+        maxGroupSize: 20,
+        groupCount: 1,
+        minGroupCount: 1,
+        maxGroupCount: 20,
+
+        set speed(newSpeed) {
+            document.getElementById("sendSpeedRange").value = newSpeed;
+            document.getElementById("sendSpeedNum").value = newSpeed;
+        }
+    }
 
 
-    document.getElementById("volumeRange").max = tone.maxVolume;
-    document.getElementById("volumeNum").min = tone.minVolume;
-    document.getElementById("volumeRange").max = tone.maxVolume;
-    document.getElementById("volumeNum").min = tone.minVolume;
 
-    document.getElementById("sendSpeedRange").min = sendOptions.minSendSpeed;
-    document.getElementById("sendSpeedRange").max = sendOptions.maxSendSpeed;
-    document.getElementById("sendSpeedNum").min = sendOptions.minSendSpeed;
-    document.getElementById("sendSpeedNum").max = sendOptions.maxSendSpeed;
+    function Letter(display, morse) {
+        this.display = display;
+        this.morse = morse;
+        this.enabled = false; 
+    }
 
-    document.getElementById("sendGroupSize").min = sendOptions.minGroupSize;
-    document.getElementById("sendGroupSize").max = sendOptions.maxGroupSize;
-    document.getElementById("sendGroupSize").value = sendOptions.groupSize;
-    document.getElementById("sendGroupsCount").min = sendOptions.minGroupCount;
-    document.getElementById("sendGroupsCount").max = sendOptions.maxGroupCount;
-    document.getElementById("sendGroupsCount").value = sendOptions.groupCount;
+    Letter.prototype.getInnerHtml = function() {
+        let listTag = document.createElement("li");
+        listTag.innerHTML = this.display;
 
-    let alphabetFieldset = document.getElementById("alphabet");
-    alphabet.forEach(function(letter) {
-        console.log(letter);
-        alphabetFieldset.appendChild(letter.getInnerHtml());
-    });
+        let inputTag = document.createElement("input");
+        inputTag.setAttribute('type','checkbox');
+        inputTag.addEventListener('click',this.toggle.bind(this));
+        listTag.appendChild(inputTag);
+        return listTag;
+    };
 
-    //Default values, forces setters to be called. 
-    tone.frequency = 440;
-    tone.volume = 50;
-    sendOptions.speed = 15;
-}
+    Letter.prototype.toggle = function() {
+        this.enabled = !this.enabled;
+        if (this.enabled) {
+            enabledAlphabet.push(this); 
+            console.log(this);
+        }  else {
+            let idx = enabledAlphabet.indexOf(this);
+            enabledAlphabet.splice(idx, 1);
+        }
+        console.log(enabledAlphabet);
 
-init();
+    };
+
+    var alphabet = [
+        new Letter("a",".-"),
+        new Letter("b","-..."),
+        new Letter("c","-.-."),
+        new Letter("d","-.."),
+        new Letter("e","."),
+        new Letter("f","..-.")
+    ];
+
+    var enabledAlphabet = [];
+
+    function sendRandomLetter() {
+        if (enabledAlphabet.length > 0) {
+            let rand = Math.random();
+            let scaledRand = rand * enabledAlphabet.length;
+            let idx = Math.floor(scaledRand);
+            console.log(enabledAlphabet[idx]);
+            alert(enabledAlphabet[idx].display);
+        } else {
+            alert("No letters selected");
+        }
+    }
+
+    function init() {
+        document.getElementById("frequencyRange").min = tone.frequencyMin;
+        document.getElementById("frequencyNum").min = tone.frequencyMin;
+        document.getElementById("frequencyRange").max = tone.frequencyMax;
+        document.getElementById("frequencyNum").max = tone.frequencyMax;
 
 
+        document.getElementById("volumeRange").max = tone.maxVolume;
+        document.getElementById("volumeNum").min = tone.minVolume;
+        document.getElementById("volumeRange").max = tone.maxVolume;
+        document.getElementById("volumeNum").min = tone.minVolume;
+
+        document.getElementById("sendSpeedRange").min = sendOptions.minSendSpeed;
+        document.getElementById("sendSpeedRange").max = sendOptions.maxSendSpeed;
+        document.getElementById("sendSpeedNum").min = sendOptions.minSendSpeed;
+        document.getElementById("sendSpeedNum").max = sendOptions.maxSendSpeed;
+
+        document.getElementById("sendGroupSize").min = sendOptions.minGroupSize;
+        document.getElementById("sendGroupSize").max = sendOptions.maxGroupSize;
+        document.getElementById("sendGroupSize").value = sendOptions.groupSize;
+        document.getElementById("sendGroupsCount").min = sendOptions.minGroupCount;
+        document.getElementById("sendGroupsCount").max = sendOptions.maxGroupCount;
+        document.getElementById("sendGroupsCount").value = sendOptions.groupCount;
+
+        let alphabetFieldset = document.getElementById("alphabet");
+        alphabet.forEach(function(letter) {
+            console.log(letter);
+            alphabetFieldset.appendChild(letter.getInnerHtml());
+        });
+
+        //Default values, forces setters to be called. 
+        tone.frequency = 440;
+        tone.volume = 50;
+        sendOptions.speed = 15;
+    }
+
+    return {
+        init:init,
+        updateFrequency:updateFrequency,
+        updateVolume:updateVolume,
+        updateSendSpeed:updateSendSpeed,
+        updateGroupSize:updateGroupSize,
+        updateGroupCount:updateGroupCount,
+        sendRandomLetter:sendRandomLetter,
+        tone:tone,
+        sendOptions:sendOptions,
+        enabledAlphabet:enabledAlphabet, 
+
+    }
+
+}();
+
+morseTutorUi.init();
